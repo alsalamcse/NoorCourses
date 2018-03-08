3ff53f4b7c9def78ee6c06687dc67c920e99049e
user <noor@danon.tzafonet.org.il>
2018-03-08 12:48:53 +0200

    dsaf

--- app/src/main/java/com/awad/noor/noorcourses/Searchforcourse.java
+++ app/src/main/java/com/awad/noor/noorcourses/Searchforcourse.java
@@ -2,10 +2,13 @@ package com.awad.noor.noorcourses;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
-public class Searchforcourse extends AppCompatActivity {
+public class Searchforcourse extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,19 @@ public class Searchforcourse extends AppCompatActivity {
         ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this,R.array.subjects,android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        String text= parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
